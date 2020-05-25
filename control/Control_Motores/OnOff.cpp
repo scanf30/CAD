14a4ebfff199cd4fbf86edc648f1dd73fd5a489d
Iván Alejandro Ochoa González <ivan.aochoa.g@gmail.com>
2020-05-25 13:00:19 -0500

    Update on/off
    se agregan los comentarios

--- control/Control_Motores/OnOff.cpp
+++ control/Control_Motores/OnOff.cpp
@@ -1,17 +1,27 @@
 #include <bits/stdc++.h>
 
+//Creamos la clase del controlador
 class OnOff
 {
 private:
 public:
+    // se definen las funciones de la clase
+    
+    // init
     OnOff();
+    
+   // funcion del controlador,
     int OnOffController( int state);
 };
-
+// función init
 OnOff::OnOff(){}
+
+//función on/off
 int OnOff::OnOffController(int state){
+    //Si el sensor esta prendido prende el actuador
     if (state)
         return 1;
+    //Si el sensor esta apagado el acutador se apaga
     else
         return 0;
 }
